18310d13a7933ac894e7df801ee5f9193ac6e672
linqingyang <2829004293@qq.com>
2024-11-21 23:50:32 +0800

    There is some unknow bugs in single combat mode.

---
+++ CMakeFiles/snake.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/linqinyang/Gluttony_Snake/src/a_star.cpp" "CMakeFiles/snake.dir/src/a_star.cpp.o" "gcc" "CMakeFiles/snake.dir/src/a_star.cpp.o.d"
+  "/home/linqinyang/Gluttony_Snake/src/engine.cpp" "CMakeFiles/snake.dir/src/engine.cpp.o" "gcc" "CMakeFiles/snake.dir/src/engine.cpp.o.d"
+  "/home/linqinyang/Gluttony_Snake/src/game.cpp" "CMakeFiles/snake.dir/src/game.cpp.o" "gcc" "CMakeFiles/snake.dir/src/game.cpp.o.d"
+  "/home/linqinyang/Gluttony_Snake/src/main.cpp" "CMakeFiles/snake.dir/src/main.cpp.o" "gcc" "CMakeFiles/snake.dir/src/main.cpp.o.d"
+  "/home/linqinyang/Gluttony_Snake/src/node.cpp" "CMakeFiles/snake.dir/src/node.cpp.o" "gcc" "CMakeFiles/snake.dir/src/node.cpp.o.d"
+  "/home/linqinyang/Gluttony_Snake/src/snake.cpp" "CMakeFiles/snake.dir/src/snake.cpp.o" "gcc" "CMakeFiles/snake.dir/src/snake.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
